895ec70148201e60f2a708b729ed709015242b03
takeuchisatoshi <satsitt3@gmail.com>
2019-03-10 19:07:00 +0900

    かけ算九九の修正

--- kuku_1.py
+++ kuku_1.py
@@ -1,14 +1,8 @@
 x = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 y = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
+for x1 in x:
 
-
-
-    for ?? in x:
-        x *= y
-    y += 1
-
-
-x * y,
-
-print(**)
+abc = 1
+abc *= x[x1]
+print(abc)
